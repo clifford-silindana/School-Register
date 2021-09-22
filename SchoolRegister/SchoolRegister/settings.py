effd60b40d27e357927b158c7e4cd45d1dcb0035
clifford <cliffords2105@gmail.com>
2021-09-22 21:16:23 +0200

    Models and urls were created.

--- SchoolRegister/SchoolRegister/settings.py
+++ SchoolRegister/SchoolRegister/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "register.apps.RegisterConfig"
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
--- SchoolRegister/register/models.py
+++ SchoolRegister/register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Learner(models.Model):
+    name = models.CharField(max_length = 100)
+    surname = models.CharField(max_length = 100)
+    grade = models.IntegerField()
+    educator = models.CharField(max_length = 100)
+    
